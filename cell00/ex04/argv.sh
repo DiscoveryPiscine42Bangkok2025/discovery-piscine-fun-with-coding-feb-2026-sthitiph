866b3c54f53d29498744c2475097fbbfaebbdc04
sthitiph <67051184@kmitl.ac.th>
2026-02-03 18:41:44 +0700

    argv.sh

--- cell00/ex04/argv.sh
+++ cell00/ex04/argv.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-for arg in "$@"
-do
-    echo "$arg"
-done
+if [ $# -eq 0 ]; then
+	echo "No arguments supplied"
+else
+	for ((i = 1; i <= $#; i++)); do
+		if [ $i -le 4 ]; then
+			echo "${!i}"
+		fi
+	done
+fi
